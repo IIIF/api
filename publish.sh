668ee9ebf0dbb8697be808f127b01a0c5d20db17
Rob Sanderson <azaroth42@gmail.com>
2014-06-03 09:34:33 -0700

    iiif.io versions of jekyll and publish.sh, Adding a vanilla publish script

--- publish.sh
+++ publish.sh
@@ -6,6 +6,16 @@
 #
 # Usage: ./publish.sh /var/www/mysite
 
+export PATH="/usr/local/rvm/gems/ruby-2.1.2/bin:/usr/local/rvm/gems/ruby-2.1.2@global/bin:/usr/local/rvm/rubies/ruby-2.1.2/bin:/usr/lib64/qt-3.3/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/usr/local/rvm/bin:/home/iiif/bin"
+export rvm_bin_path="/usr/local/rvm/bin"
+export GEM_HOME="/usr/local/rvm/gems/ruby-2.1.2"
+export IRBRC="/usr/local/rvm/rubies/ruby-2.1.2/.irbrc"
+export MY_RUBY_HOME="/usr/local/rvm/rubies/ruby-2.1.2"
+export rvm_path="/usr/local/rvm"
+export rvm_prefix="/usr/local"
+export GEM_PATH="/usr/local/rvm/gems/ruby-2.1.2:/usr/local/rvm/gems/ruby-2.1.2@global"1
+export rvm_version="1.25.27 (stable)"
+
 dir=$1
 
 if [ $dir"X" == "X" ]; then
@@ -17,4 +27,5 @@ fi
 if [ -d $dir ]; then
   rm -rf $dir/*
 fi
-jekyll build -d $dir
+./myjekyll build -d $dir
+#jekyll build -d $dir
