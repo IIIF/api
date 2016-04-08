b4f9cb359b3d975aae60988f9b1ec4da517f2d01
Jon Stroop <jpstroop@gmail.com>
2016-04-08 10:39:34 +0100

    update hash fragment when we scroll

--- source/js/anchor.js
+++ source/js/anchor.js
@@ -1,4 +1,15 @@
+// add anchor images and links when hovering over anchors
 window.onload = function() {
   anchors.options.placement = 'left';
   anchors.add();
 };
+
+// update the hash fragment as we scroll
+$(document).bind('scroll', function(e) {
+  $('h2,h3,h4').each(function() {
+    if ($(this).offset().top < window.pageYOffset + 5 && $(this).offset().top + $(this).height() > window.pageYOffset + 5) {
+      var urlId = '#' + $(this).attr('id');
+      window.history.replaceState(null, null, urlId);
+    }
+  });
+});
