a33cb15765455c100bbc38df771e49e89fe940aa
Jon Stroop <jpstroop@gmail.com>
2017-09-27 11:40:23 -0700

    refactor kramdown spec

--- spec/source/syntax_spec.rb
+++ spec/source/syntax_spec.rb
@@ -1,26 +1,47 @@
 require 'spec_helper'
 require 'kramdown/document'
 
+DO_NOT_CHECK = %w{acronyms.md links.md}
+
+def check_doc(src_str)
+  # returns a hash of errors we care about
+  r = /link ID '[a-zA-Z][_0-9a-zA-Z-]*'/
+  Kramdown::Document.new(src_str).warnings.grep(r)
+end
+
+def has_links_include?(src_str)
+  src_str.include? "{% include links.md %}"
+end
+
+def links_include
+  @links_include_path ||= File.expand_path('../../../source/_includes/links.md', __FILE__)
+  @links_include ||= File.open(@links_include_path, 'rb').read
+end
+
+def add_links(src_str)
+  [src_str, links_include].join('').gsub(/{% include links.md %}/, '')
+end
+
+def markdown_files
+  glob_pattern = File.join(File.expand_path('../../../source', __FILE__), '**/*.md')
+  Dir.glob(glob_pattern).reject do |path|
+    DO_NOT_CHECK.any?{ |name| path.end_with? name }
+  end
+end
+
 describe 'Editors' do
 
   they 'tend to screw up markdown' do
-
     report = {}
-    glob_pattern = File.join(File.expand_path('../../../source', __FILE__), '**/*.md')
-    markdown_files = Dir.glob(glob_pattern)
-    links_include_path = File.expand_path('../../../source/_includes/links.md', __FILE__)
-    links_include = File.open(links_include_path, 'rb').read
     markdown_files.each do |mf|
-      unless %w{acronyms.md links.md}.any? { |inclood| mf.end_with? inclood }
-        File.open(mf) do |f|
-          src = f.read
-          if src.include? "{% include links.md %}"
-            src = [src, links_include].join('').gsub(/{% include links.md %}/, '')
-          end
-          errors = Kramdown::Document.new(src).warnings.grep(/link ID '[a-zA-Z][_0-9a-zA-Z-]*'/)
-          unless errors.length == 0
-            report[mf] = errors
-          end
+      File.open(mf) do |f|
+        src = f.read
+        if has_links_include?(src)
+          src = add_links(src)
+        end
+        errors = check_doc(src)
+        unless errors.length == 0
+          report[mf] = errors
         end
       end
     end
