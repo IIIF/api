5657a556cfef8ec34c271a423094e408f0f16641
Simeon Warner <simeon.warner@cornell.edu>
2018-04-04 13:41:40 -0400

    grunt fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,8 @@ module.exports = function(grunt) {
             /cc:attributionURL/,
             /Attribute “rel” not allowed on element “span”/,
             /Consider using the “h1” element/,
-				/Attribute “integrity” not allowed on element “script”/
+            /Attribute “integrity” not allowed on element “script”/,
+            /This document appears to be written in French but the/
           ]
         },
         src : '_site/**/*.html'
